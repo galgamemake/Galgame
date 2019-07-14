b4e70183c21868dfc1198799ffa6c11c6d7faa58
SonaruIsuge <bread511206@gmail.com>
2019-07-14 18:01:29 +0800

    更新fungus插件
    最後BGM bug尚未改善

--- Assets/Fungus/Scripts/Editor/FungusEditorPreferences.cs
+++ Assets/Fungus/Scripts/Editor/FungusEditorPreferences.cs
@@ -17,42 +17,55 @@ namespace Fungus
             // Have we loaded the prefs yet
             private static bool prefsLoaded = false;
             const string HIDE_MUSH_KEY = "hideMushroomInHierarchy";
-            const string USE_EXP_MENUS = "useExperimentalMenus";
+            const string USE_LEGACY_MENUS = "useLegacyMenus";
 
             public static bool hideMushroomInHierarchy;
-            public static bool useExperimentalMenus;
+            public static bool useLegacyMenus;
 
             static FungusEditorPreferences()
             {
                 LoadOnScriptLoad();
             }
 
-            // Add preferences section named "My Preferences" to the Preferences Window
-            [PreferenceItem("Fungus")]
-            public static void PreferencesGUI()
+            [SettingsProvider]
+            public static SettingsProvider CreateFungusSettingsProvider()
             {
-                // Load the preferences
-                if (!prefsLoaded)
+                // First parameter is the path in the Settings window.
+                // Second parameter is the scope of this setting: it only appears in the Project Settings window.
+                var provider = new SettingsProvider("Project/Fungus", SettingsScope.Project)
                 {
-                    LoadOnScriptLoad();
-                }
+                    // Create the SettingsProvider and initialize its drawing (IMGUI) function in place:
+                    guiHandler = (searchContext) =>
+                    {
+                        // Load the preferences
+                        if (!prefsLoaded)
+                        {
+                            LoadOnScriptLoad();
+                        }
 
-                // Preferences GUI
-                hideMushroomInHierarchy = EditorGUILayout.Toggle("Hide Mushroom Flowchart Icon", hideMushroomInHierarchy);
-                useExperimentalMenus = EditorGUILayout.Toggle(new GUIContent("Experimental Searchable Menus", "Experimental menus replace the Event, Add Variable and Add Command menus with a searchable menu more like the Unity AddComponent menu."), useExperimentalMenus);
+                        // Preferences GUI
+                        hideMushroomInHierarchy = EditorGUILayout.Toggle("Hide Mushroom Flowchart Icon", hideMushroomInHierarchy);
+                        useLegacyMenus = EditorGUILayout.Toggle(new GUIContent("Legacy Menus", "Force Legacy menus for Event, Add Variable and Add Command menus"), useLegacyMenus);
 
-                // Save the preferences
-                if (GUI.changed)
-                {
-                    EditorPrefs.SetBool(HIDE_MUSH_KEY, hideMushroomInHierarchy);
-                    EditorPrefs.SetBool(USE_EXP_MENUS, useExperimentalMenus);
-                }
+                        // Save the preferences
+                        if (GUI.changed)
+                        {
+                            EditorPrefs.SetBool(HIDE_MUSH_KEY, hideMushroomInHierarchy);
+                            EditorPrefs.SetBool(USE_LEGACY_MENUS, useLegacyMenus);
+                        }
+                    },
+
+                    // // Populate the search keywords to enable smart search filtering and label highlighting:
+                    // keywords = new HashSet<string>(new[] { "Number", "Some String" })
+                };
+
+                return provider;
             }
 
             public static void LoadOnScriptLoad()
             {
                 hideMushroomInHierarchy = EditorPrefs.GetBool(HIDE_MUSH_KEY, false);
-                useExperimentalMenus = EditorPrefs.GetBool(USE_EXP_MENUS, false);
+                useLegacyMenus = EditorPrefs.GetBool(USE_LEGACY_MENUS, false);
                 prefsLoaded = true;
             }
         }
